e93dae64dbc2f41835e0718075f825d0bc49a055
Ikenna <ikennaugwuh@gmail.com>
2016-06-24 16:01:35 -0400

    Added CSS, edited a bit of markup.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,18 @@
 import {results} from './results';
 import $ from 'jquery';
 
+$('button').click(function(e){ e.preventDefault(); });
+
 var resultsContainer = $('.results');
 results.results.forEach(function(r){
     var n =
     `
         <li>
-            <a href="${r.url}"><img src="${r.Images[0].url_570xN}"></a>
-            <div>
+            <div class="image">
+                <img src="http://placehold.it/34x27">
+                <a href="${r.url}"><img src="${r.Images[0].url_570xN}"></a>
+            </div>
+            <div class="info">
                 <span class="result-title">${r.title}</div>
                 <span class="shop-name">${r.Shop.shop_name}</span>
                 <span class="price">$${r.price}</span>
