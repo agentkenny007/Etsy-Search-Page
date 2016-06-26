0de5535f00ea29aeb34e75f6cabc243ddfc11b6d
Ikenna <ikennaugwuh@gmail.com>
2016-06-26 14:11:24 -0400

    Adjusted markup and styles.

--- src/js/main.js
+++ src/js/main.js
@@ -3,23 +3,29 @@ import $ from 'jquery';
 
 $('button').click(function(e){ e.preventDefault(); });
 
+$('.results-info .left').append(`<span class="search-term">${results.params.keywords}</span> (${results.count} Results)`);
+
 var resultsContainer = $('.results');
 results.results.forEach(function(r){
-    var n =
-    `
+    resultsContainer.append(`
         <li>
-            <div class="image">
-                <img src="http://placehold.it/34x27">
-                <a href="${r.url}"><img src="${r.Images[0].url_570xN}"></a>
-            </div>
-            <div class="info">
-                <span class="result-title">${r.title}</div>
-                <span class="shop-name">${r.Shop.shop_name}</span>
-                <span class="price">$${r.price}</span>
-            </div>
+            <a href="${r.url}" title="${r.title}">
+                <div class="image">
+                    <img src="http://placehold.it/34x27">
+                    <span style="background: url(${r.Images[0].url_570xN}) center/cover no-repeat;"></span>
+                </div>
+                <div class="info">
+                    <span class="result-title">${r.title}</span>
+                    <span class="shop-name">${r.Shop.shop_name}</span>
+                    <span class="price">$${r.price}</span>
+                </div>
+                <div class="item">
+                    <span class="fav"></span>
+                    <span class="share"></span>
+                </div>
+            </a>
         </li>
-    `;
-    resultsContainer.append(n);
+    `);
 });
 
 var D = new Date();
